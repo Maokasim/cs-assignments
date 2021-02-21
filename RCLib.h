46f7ce3a33a2fca0d04bf695a25db541e164dc25
Maokasim <77733487+Maokasim@users.noreply.github.com>
2021-02-21 19:04:51 +0200

    My own library

---
+++ RCLib.h
@@ -0,0 +1,7 @@
+#ifndef RC_LIB_H
+#define RC_LIB_H
+
+#include<Arduino.h>
+int sum (int,int);
+
+#endif
---
+++ RCLib.ino
@@ -0,0 +1,10 @@
+#include "RCLib.h"
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(sum(174,243));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
